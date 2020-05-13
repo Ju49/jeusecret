2c503c95b19475b4c58b7f21296f29ecf0775933
Cynism <axelfg@sfr.fr>
2020-05-13 18:38:37 +0200

    Edit
    Co-Authored-By: greenwasher <greenwasher@users.noreply.github.com>

--- Jeu/server.R
+++ Jeu/server.R
@@ -2,10 +2,8 @@ library(shiny)
 
 server <- function(input, output,session){
   
-  #col <- as.numeric(input$repUser)
-  
   observeEvent(input$btnRep1,
-                output$txtRep1 <- renderText(dataRes[r,isolate(input$repUser+1)])
+                output$txtRep1 <- renderText(dataRes[r,isolate(input$repUser+1)]),
                )
 }
                
