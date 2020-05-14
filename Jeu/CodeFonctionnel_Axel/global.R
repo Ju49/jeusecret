f3258333fb849278139084e1fbe168ad5fb64bd8
Cynism <axelfg@sfr.fr>
2020-05-14 16:53:53 +0200

    Répartition des dossiers d'amélioration
    Co-Authored-By: greenwasher <greenwasher@users.noreply.github.com>
    Co-Authored-By: ju49 <ju49@users.noreply.github.com>
    Co-Authored-By: chalarock <chalarock@users.noreply.github.com>

---
+++ Jeu/CodeFonctionnel_Axel/global.R
@@ -0,0 +1,14 @@
+library(shiny)
+
+dataOrigin <- read.csv("TestEch.csv",sep=";",header=TRUE);dataOrigin
+dataTest <- dataOrigin[sample(1:nrow(dataOrigin),5),];dataTest
+
+source('ui.R', local = TRUE)
+source('server.R')
+
+
+shinyApp(
+  ui = ui,
+  server = server
+)
+  
